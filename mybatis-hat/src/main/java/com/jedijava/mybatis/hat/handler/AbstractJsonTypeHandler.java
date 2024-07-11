2623c0cf2a7fb39ee6a05c609cea91308a10d924
liukaiyang <kelefun@foxmail.com>
2024-07-11 23:48:18 +0800

    json type handler

---
+++ mybatis-hat/src/main/java/com/jedijava/mybatis/hat/handler/AbstractJsonTypeHandler.java
@@ -0,0 +1,41 @@
+package com.jedijava.mybatis.hat.handler;
+
+import com.jedijava.mybatis.hat.utils.HatStringUtil;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class AbstractJsonTypeHandler<T> extends BaseTypeHandler<T> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, toJson(parameter));
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        final String json = rs.getString(columnName);
+        return HatStringUtil.isBlank(json) ? null : parse(json);
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        final String json = rs.getString(columnIndex);
+        return HatStringUtil.isBlank(json) ? null : parse(json);
+    }
+
+    @Override
+    public T getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        final String json = cs.getString(columnIndex);
+        return HatStringUtil.isBlank(json) ? null : parse(json);
+    }
+
+    protected abstract T parse(String json);
+
+    protected abstract String toJson(T object);
+
+}
---
+++ mybatis-hat/src/main/java/com/jedijava/mybatis/hat/handler/JacksonTypeHandler.java
@@ -0,0 +1,57 @@
+package com.jedijava.mybatis.hat.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+import org.springframework.util.Assert;
+
+import java.io.IOException;
+
+@Slf4j
+@MappedTypes({Object.class})
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class JacksonTypeHandler extends AbstractJsonTypeHandler<Object> {
+    private static ObjectMapper OBJECT_MAPPER;
+    private final Class<?> type;
+
+    public JacksonTypeHandler(Class<?> type) {
+        if (log.isTraceEnabled()) {
+            log.trace("JacksonTypeHandler(" + type + ")");
+        }
+        Assert.notNull(type, "Type argument cannot be null");
+        this.type = type;
+    }
+
+    @Override
+    protected Object parse(String json) {
+        try {
+            return getObjectMapper().readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected String toJson(Object obj) {
+        try {
+            return getObjectMapper().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        if (null == OBJECT_MAPPER) {
+            OBJECT_MAPPER = new ObjectMapper();
+        }
+        return OBJECT_MAPPER;
+    }
+
+    public static void setObjectMapper(ObjectMapper objectMapper) {
+        Assert.notNull(objectMapper, "ObjectMapper should not be null");
+        JacksonTypeHandler.OBJECT_MAPPER = objectMapper;
+    }
+}
